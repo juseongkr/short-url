5b495781a5e65ca3706df4e628a6387ffbc8d1f6
Juseong Park <juseongkr@gmail.com>
2020-12-03 11:56:53 +0900

    Create: auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/decorator/get-user';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe)
+    authDto: AuthDto,
+  ): Promise<void> {
+    return this.authService.signUp(authDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe)
+    authDto: AuthDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authDto);
+  }
+
+  @Post('/check')
+  @UseGuards(AuthGuard())
+  check(
+    @GetUser()
+    user: User,
+  ): void {
+    console.log(user);
+  }
+}
